715876c21735b85dfc97a121755bee30ae428967
DESKTOP-TIDRHG5\ruize <nalanchangge@gmail.com>
2022-03-25 16:53:31 +0100

    First test of http

--- src/web_service/Main.java
+++ src/web_service/Main.java
@@ -1,7 +1,38 @@
 package web_service;
 
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.*;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.net.URIBuilder;
+import org.json.JSONObject;
+import org.json.XML;
+
+import javax.swing.text.html.parser.Entity;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException, IOException, ParseException {
         System.out.println("Ciao Pretto");
+        CloseableHttpClient client = HttpClients.createDefault();
+        URIBuilder ubi = new URIBuilder("https://www.googleapis.com/books/v1/volumes");
+        ubi.addParameter("q","pride+prejudice");
+        ubi.addParameter("download","epub");
+        ubi.addParameter("key","AIzaSyBjMDhk3r9psbNzULGuS5in0IoIP6XYj-U");
+        URI uri = ubi.build();
+        HttpGet get = new HttpGet(uri);
+        ClassicHttpResponse response = client.execute(get);
+        HttpEntity entity = response.getEntity();
+        String str = EntityUtils.toString(entity);
+        System.out.println(str);
+
+
+
+
     }
 }
